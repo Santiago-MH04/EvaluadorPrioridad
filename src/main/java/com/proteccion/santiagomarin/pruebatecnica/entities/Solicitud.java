7d029523b20dccae48694cff3f41f5dfe4458342
Santiago Marín Higuita <146114200+Santiago-MH04@users.noreply.github.com>
2026-01-08 09:40:59 -0500

    Definición de las entidades de dominio (#1)
    * feat: project creation and connected both to SQL and NoSQL databases
    
    * feat: environment variables included, in order to secure the project
    
    * feat: crear entidades JPA Solicitud y Usuario
    
    ✅ Solicitud: UUID, tipo enum, prioridadManual (default=1)
    ✅ Usuario: Long ID, nombre, apellido, correo único
    ✅ Relación @ManyToOne correcta
    ✅ @PrePersist para fechaCreacion y prioridadManual
    ✅ Paquete entities/ bien organizado
    
    Listo para: Repositorios + Motor de reglas

---
+++ src/main/java/com/proteccion/santiagomarin/pruebatecnica/entities/Solicitud.java
@@ -0,0 +1,40 @@
+package com.proteccion.santiagomarin.pruebatecnica.entities;
+
+import com.proteccion.santiagomarin.pruebatecnica.entities.utils.TipoSolicitud;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "solicitudes")
+@Data
+public class Solicitud {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoSolicitud tipo;
+
+    @Column(name = "prioridad_manual")
+    private Integer prioridadManual;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.fechaCreacion == null) {
+            this.fechaCreacion = LocalDateTime.now();
+        }
+        if (this.prioridadManual == null) {
+            this.prioridadManual = 1;
+        }
+    }
+}
---
+++ src/test/java/com/proteccion/santiagomarin/pruebatecnica/PruebatecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proteccion.santiagomarin.pruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebatecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
