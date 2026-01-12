e3f4f1d8573ac7c3ca9dbda05e9dc0cfc0f913b7
Santiago <santimh04@gmail.com>
2026-01-11 21:35:05 -0500

    feat: basic authentication provided. Pending to migrate to JWT

--- src/main/java/com/proteccion/santiagomarin/pruebatecnica/security/SecurityConfig.java
+++ src/main/java/com/proteccion/santiagomarin/pruebatecnica/security/SecurityConfig.java
@@ -1,15 +1,56 @@
 package com.proteccion.santiagomarin.pruebatecnica.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return this.authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/api/tickets/prioritised").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/tickets/crear").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/usuario/registrar").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults()) // autenticación básica
+            .csrf(AbstractHttpConfigurer::disable)
+                /*.csrf(csrf -> csrf.disable())*/
+            /*.cors(cors -> cors.configurationSource(this.corsConfigurationSource()))*/
+            .build();
     }
 }
